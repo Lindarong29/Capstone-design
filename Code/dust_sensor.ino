50b12cd9302f3205b9001651da8316dae06fc1cd
Lindarong <76677316+Lindarong29@users.noreply.github.com>
2021-09-14 11:20:26 +0900

    Update dust_sensor.ino

--- Code/dust_sensor.ino
+++ Code/dust_sensor.ino
@@ -7,13 +7,13 @@ int state=0; //현재 상태
 int pre_state=0; //이전 상태
 
 //미세먼지
-int vo = A5; //OUT핀 - A5
+int vo = A5; //input핀 - A5
 int v_led = 2; //미세먼지 내부 led - D2
 float vo_value = 0; //센서가 Read 한 Analog value (0~1023)
 float Voltage = 0; //Analog -> Voltage 변환 값
 float dust_density = 0; //미세먼지 농도 값
 float dust_sum = 0; //미세먼지 값 저장 (10번 측정)
-
+int dust_avg = 0; //
 
 void setup() {
   // put your setup code here, to run once:
@@ -26,38 +26,10 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
-  //미세먼지 측정
-  digitalWrite(v_led, LOW); // 미세먼지 센서 내의 LED ON
-  delayMicroseconds(280); // 280us Delay
-  vo_value = analogRead(A5); // 미세먼지 측정 값 READ (Analog)
-  delayMicroseconds(40); // 40us Delay
-  digitalWrite(v_led, HIGH); // 미세먼지 센서 내의 LED OFF
-  delayMicroseconds(9680);  // 9680us Delay
-
-  Voltage = vo_value * 5.0 / 1023.0 ; // 아날로그 값을 전압으로 변환
-  dust_density = (Voltage - no_dust) / 0.005;  // 전압 이용하여 미세먼지 농도 계산
-
-
-  //빗방울센서 측정
-  rain_value = analogRead(A4); //빗방울 센서 READ (A4)
-
-
-  //시리얼 출력
-  Serial.print("Dust Density : ");
-  Serial.print(dust_density);
-  Serial.print(", rain : ");
-  Serial.print(rain_value);
-
-
-  if(dust_density >400 || rain_value < 500) state == 1;
   
   for(int i=0; i<11; i++) { //10번 측정
-
-   //빗물
-    rain_value = analogRead(A0);
-    rain_sum += rain_value; // 측정한 빗물센서 값 저장
-
-   //미세먼지
+    
+  //미세먼지 측정
     digitalWrite(v_led, LOW); // 미세먼지 센서 내의 LED ON
     delayMicroseconds(280); // 280us Delay
     vo_value = analogRead(A5); // 미세먼지 측정 값 READ (Analog)
@@ -67,16 +39,18 @@ void loop() {
 
     Voltage = vo_value * 5.0 / 1023.0 ; // 아날로그 값을 전압으로 변환
     dust_density = (Voltage - no_dust) / 0.005;  // 전압 이용하여 미세먼지 농도 계산
+
     dust_sum += dust_density; //측정한 미세먼지 값 저장
 
     delay(500);
   }
-
+  
+  
   //10번 측정한 값 평균 구함
-  rain_sum/=10;
-  dust_sum/=10;
+ 
+  dust_avg = dust_sum/10;
 
-  if(rain_sum >900 && dust_sum <200) { //기준에 맞으면 state=0
+  if(dust_sum <200) { //기준에 맞으면 state=0
     state = 0;
     Serial.println("Weather is good for 5s");
   }
