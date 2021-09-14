d970bb25a4502842c2a262454031f0e5bf27a2c1
Lindarong <76677316+Lindarong29@users.noreply.github.com>
2021-09-14 11:52:01 +0900

    Update step_motoer.ino

--- Code/step_motoer.ino
+++ Code/step_motoer.ino
@@ -1,8 +1,20 @@
 #include <Stepper.h>
+
 const int stepvalue=2048;
+
 Stepper stepper(stepvalue, 8, 7, 6, 5); // stepper객체 설정
 // 연결 핀 : IN4, IN2, IN3, IN1
 
-setup
+void setup () {
   stepper.setSpeed(15); // 모터 속도
+}
+
+void loop () {
+  stepper.step(stepvalue);
+  Serial.println("open");
+  delay (1000);
   
+  stepper.step(-stepvalue);
+  Serial.println("close");
+  delay (1000);
+}
